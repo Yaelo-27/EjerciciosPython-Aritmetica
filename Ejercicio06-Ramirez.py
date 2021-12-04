4aeee62745dbddc4db018eeb1f0ad87e21c12aec
Yaelo-27 <78753436+Yaelo-27@users.noreply.github.com>
2021-12-04 19:12:42 +0100

    Create Ejercicio06-Ramirez.py

---
+++ Ejercicio06-Ramirez.py
@@ -0,0 +1,25 @@
+import math
+def hora(segundos):
+    minutos = float(segundos)/60
+    horas = minutos/60
+    horas_decimal,horas_entera = math.modf(horas)
+    minutos_decimal,minutos_entera = math.modf(minutos)
+
+    minutos_total = horas_decimal*60
+    segundos_total  = minutos_decimal*60
+
+    
+
+    print("Horas : "+str(int(horas_entera))+" minutos: "+str(int(minutos_total))+" segundos: "+str(int(segundos_total)))
+    
+    
+    
+
+segundos = input('Introduzca los segundos: ')
+
+hora(segundos)
+        
+    
+
+    
+    
